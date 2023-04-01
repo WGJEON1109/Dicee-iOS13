861568ba3930a50eb1e2f0c5d4839bb4622af2d6
WGJEON <dnrms1109@gmail.com>
2023-04-01 23:44:39 +0900

    [CRE] Create Dice Module

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,25 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    override func viewDidLoad() {
+//        // view가 로드되면(앱이 시작되면)
+//        super.viewDidLoad()
+//        // Image Literal => #imageLiteral(로 입력
+//    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        // .randomElement == [Int.random(in: 0...5)]
+        diceImageView2.image = diceArray.randomElement()
+
     }
-
-
+    
 }
 
